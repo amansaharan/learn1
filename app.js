e78d27df3252e185b8974ece9091b2dce7eaf426
Aman Saharan <amansaharan2013@gmail.com>
2023-11-10 15:08:36 +0530

    Fourth Commit after port update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 5001;
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -9,6 +10,6 @@ app.get('/intro', (req, res) => {
   res.send({ name: 'Aman Saharan', age: 35 });
 });
 
-app.listen(5001, () => {
-  console.log('App is running on PORT', 5001);
+app.listen(port, () => {
+  console.log('App is running on PORT', port);
 });
