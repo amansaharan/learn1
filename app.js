43a3080ffb4ffbdf9a6c0d8b013c7215c2a6d869
Aman Saharan <amansaharan2013@gmail.com>
2023-11-10 15:05:27 +0530

    Third Commit

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log('App is running on PORT', 5000);
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/intro', (req, res) => {
+  res.send({ name: 'Aman Saharan', age: 35 });
+});
+
+app.listen(5001, () => {
+  console.log('App is running on PORT', 5001);
 });
